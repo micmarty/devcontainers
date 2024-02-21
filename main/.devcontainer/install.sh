69872f8c0b552d9815aede0737298fbbf6a63f1f
Michał Martyniak <michal.martyniak@linux.pl>
2024-02-21 22:50:32 +0000

    use chezmoi bin instaled downloading it again

--- main/.devcontainer/install.sh
+++ main/.devcontainer/install.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 GITHUB_USERNAME=micmarty
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
+chezmoi init --apply $GITHUB_USERNAME --verbose
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 oh-my-posh font install Meslo
