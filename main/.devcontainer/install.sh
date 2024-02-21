338d5807a2232354f502dd342c5e64b9c7e0e6b2
Michał Martyniak <michal.martyniak@linux.pl>
2024-02-21 22:50:02 +0000

    install neofetch

--- main/.devcontainer/install.sh
+++ main/.devcontainer/install.sh
@@ -6,4 +6,5 @@ sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 oh-my-posh font install Meslo
+neofetch
 
