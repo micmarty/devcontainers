15f2e75eb9b7dfb9fa278483deed60e81ea17c6f
Michał Martyniak <michal.martyniak@linux.pl>
2024-02-21 23:19:19 +0000

    open tmux automatically

--- main/.devcontainer/install.sh
+++ main/.devcontainer/install.sh
@@ -6,5 +6,5 @@ chezmoi init --apply $GITHUB_USERNAME --verbose
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 oh-my-posh font install Meslo
-neofetch
+tmux new -A -s master -n default bash
 
