ec84daf181524345004e889cbaaa3825e8b52aae
Michał Martyniak <michal.martyniak@linux.pl>
2024-02-21 22:18:59 +0000

    add zoxide

--- main/.devcontainer/install.sh
+++ main/.devcontainer/install.sh
@@ -3,4 +3,5 @@
 GITHUB_USERNAME=micmarty
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
+curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 
