b9c8a66c7014e6047e93d315069271065cd3a5ea
Michał Martyniak <michal.martyniak@linux.pl>
2024-02-21 22:19:04 +0000

    add oh-my-posh

--- main/.devcontainer/install.sh
+++ main/.devcontainer/install.sh
@@ -4,4 +4,6 @@ GITHUB_USERNAME=micmarty
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+oh-my-posh font install Meslo
 
