9706937f7f5ccd72698d0afa878f2a8b9379b9c4
Michał Martyniak <michal.martyniak@linux.pl>
2024-02-22 22:20:21 +0000

    tidy

--- main/.devcontainer/install.sh
+++ main/.devcontainer/install.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
 GITHUB_USERNAME=micmarty
+# tmux tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# pyenv
 curl https://pyenv.run | bash
+
+# chezmoi dotfiles
 chezmoi init --apply $GITHUB_USERNAME --verbose
 
 mkdir -p ~/.local/bin
+# bat
 ln -s /usr/bin/batcat ~/.local/bin/bat
+
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all --xdg --no-fish --no-zsh --no-update-rc 
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 
+# zoxide
+curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+
+# oh-my-posh
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 oh-my-posh font install Meslo
-tmux new -A -s master -n default bash
+exit
+
+# tmux new -A -s master -n default bash
 
