1a513d5aada0534213dd3bac17517667ac72d907
Michał Martyniak <michal.martyniak@linux.pl>
2024-02-22 19:04:05 +0000

    add pyenv

--- main/.devcontainer/install.sh
+++ main/.devcontainer/install.sh
@@ -2,6 +2,7 @@
 
 GITHUB_USERNAME=micmarty
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+curl https://pyenv.run | bash
 chezmoi init --apply $GITHUB_USERNAME --verbose
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
